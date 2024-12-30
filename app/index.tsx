e0787e80215b3b008ff6754aca44b9813eaf47cb
Francisco Lucas <fcolucaslima14@gmail.com>
2024-12-30 18:09:33 -0300

    add components

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,7 @@
+import { Button, ButtonIcon } from "@/components/ui/button";
+import { Divider } from "@/components/ui/divider";
+import { AddIcon, TrashIcon } from "@/components/ui/icon";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { FlatList, Text, View } from "react-native";
 
 const items = [
@@ -25,13 +29,26 @@ const items = [
 
 export default function Index() {
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-white px-3">
+      <Input size="lg">
+        <InputField placeholder="Adicione um item..." />
+
+        <InputSlot className="px-2">
+          <InputIcon as={AddIcon} className="text-info-500" />
+        </InputSlot>
+      </Input>
+
       <FlatList
         data={items}
         keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <Divider className="bg-gray-300" />}
         renderItem={({ item }) => (
-          <View className="p-4">
+          <View className="py-4 flex-row flex-1 items-center justify-between">
             <Text>{item.title}</Text>
+
+            <Button action="negative" variant="outline" className="">
+              <ButtonIcon as={TrashIcon} />
+            </Button>
           </View>
         )}
       />
